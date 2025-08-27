1f45cf66a0b13c345c473facb5573ec1be2c0495
Shiva9361 <shivadharshansankar936@gmail.com>
2025-08-27 23:05:23 +0530

    feat: scripts for streamlining development

---
+++ build_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd tests
+g++ test_generator.cpp
+./a.out
+rm a.out
---
+++ build_vm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
+cd build
+make
+cd ..
+cp build/vm vm
+chmod +x vm
+echo "VM built successfully."
